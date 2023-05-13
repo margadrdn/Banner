824bb5b872a411128d3896edb4ef851fb681968c
Margad-Erdene <emeralds0214@gmail.com>
2023-05-14 02:07:22 +0800

    update

--- Sources/Banner/BannerView.swift
+++ Sources/Banner/BannerView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public struct BannerView: View {
+  
   let banner: BannerType
+  
+  public init(banner: BannerType) {
+    self.banner = banner
+  }
+  
   public var body: some View {
     ZStack {
       VStack {
