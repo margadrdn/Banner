19a0ab6105a72055e68b03bab4201585c18267dc
Margad-Erdene <emeralds0214@gmail.com>
2023-05-14 01:59:56 +0800

    update

--- Sources/Banner/BannerService.swift
+++ Sources/Banner/BannerService.swift
@@ -1,6 +1,7 @@
 import Foundation
 import SwiftUI
 
+@available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public class BannerService: ObservableObject {
   public static let shared = BannerService()
